27df71e1d092eaa9b5e429586be3e51fd5490c2c
Kevin Kang <kkang@berkeley.edu>
2016-02-27 11:55:33 -0800

    twilio makes post and get requests from /order now

--- runserver.py
+++ runserver.py
@@ -4,6 +4,9 @@ This script runs the FlaskWebProject application using a development server.
 
 from os import environ
 from FlaskWebProject import app
+from flask import request, redirect
+import twilio.twiml
+import urllib, httplib
 
 if __name__ == '__main__':
     HOST = environ.get('SERVER_HOST', 'localhost')
@@ -12,3 +15,21 @@ if __name__ == '__main__':
     except ValueError:
         PORT = 5555
     app.run(HOST, PORT)
+ 
+@app.route("/order", methods=['GET', 'POST'])
+def inTextOutStock():
+    """Parse incoming texts and send JSON sell/buy order."""
+    incomingBody = request.values.get('Body', None)
+    parsedBody = inomingMessage.split(" ")
+    if (parsedBody[0] == "B" || parsedBody[0] == "S") {
+    	params = {
+    		'order': parsedBody[0],
+    		'sym' : parsedBody[1],
+    		'val' : parsedBody[2]
+    		}
+        return str(params);
+    } else {
+    	#not valid buy/sell order
+    	return;
+    }
+    return;
