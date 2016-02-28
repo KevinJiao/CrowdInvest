35fecbccb58221f58cb5130d2e5dc9d24b4cf0e8
Kevin Jiao <kevin.jiao@berkeley.edu>
2016-02-27 21:55:40 -0800

    fix buy

--- FlaskWebProject/utils.py
+++ FlaskWebProject/utils.py
@@ -80,7 +80,7 @@ def buy(sym, val, g):
         g.db.commit()
         funds = g.db.execute("SELECT sym, amount FROM portfolio WHERE sym = ?", ["funds"]).fetchall()[0][1]
         cost = quote * float(val)
-        if funds < cost:
+        if float(funds) < cost:
             return
         g.db.execute("INSERT OR IGNORE INTO portfolio VALUES (?,?)", [sym, 0])
         g.db.execute("UPDATE portfolio SET amount = amount + ? WHERE sym = ?", [val, sym])
